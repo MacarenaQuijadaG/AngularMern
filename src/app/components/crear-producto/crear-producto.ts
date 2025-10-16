615e7ac2bc5e4442a6bda8b0c6a1c8370d464271
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2025-10-16 05:23:34 -0300

    Se modificó el flujo de navegación de CrearProducto a ListarProductos, se implementó el uso de queryParams para pasar el mensaje de éxito, se actualizó ListarProductos para mostrar la alerta de Bootstrap, la alerta desaparece automáticamente después de 3 segundos, esto reemplaza el uso de getCurrentNavigation(), evitando deprecaciones

--- src/app/components/crear-producto/crear-producto.ts
+++ src/app/components/crear-producto/crear-producto.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Producto } from '../../models/producto'
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-crear-producto',
-  imports: [RouterLink],
+  standalone: true,                                
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './crear-producto.html',
-  styleUrl: './crear-producto.css'
+  styleUrls: ['./crear-producto.css']
 })
-export class CrearProducto {
+export class CrearProducto implements OnInit {
+  productoForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router:Router) {}
+
+  ngOnInit(): void {
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  mensajeExito: string | null = null;
+ guardar() {
+    if (this.productoForm.valid) {
+      
+      const PRODUCTO: Producto = new Producto(
+        this.productoForm.get('producto')?.value || '',
+        this.productoForm.get('categoria')?.value || '',
+        this.productoForm.get('ubicacion')?.value || '',
+        this.productoForm.get('precio')?.value || 0
+      );
+
+      console.log(PRODUCTO);
+      this.router.navigate(['/'], { 
+      queryParams: { mensajeExito: 'El producto fue registrado con éxito!' }
+
+    });
+  } else {
+    alert('El formulario no es válido');
+  }
 }
+  }
+
+
--- src/app/components/listar-productos/listar-productos.ts
+++ src/app/components/listar-productos/listar-productos.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-listar-productos',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule], 
   templateUrl: './listar-productos.html',
   styleUrls: ['./listar-productos.css']
 })
-export class ListarProductos {
 
+export class ListarProductos implements OnInit {
+  mensajeExito: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.mensajeExito = params['mensajeExito'] || null;
+
+      if (this.mensajeExito) {
+        setTimeout(() => this.mensajeExito = null, 5000);
+      }
+    });
+  }
 }
+ 
